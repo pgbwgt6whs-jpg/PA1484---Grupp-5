d40bb0e16853a9750083c747fea83459c89261c8
elsaav <elvo24@student.bth.se>
2026-09-23 11:22:35 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -42,7 +42,7 @@ class Application:
         self.tile2 = self.tileview.add_tile(1, 0, lv.DIR.LEFT)
 
         self.tile1_label = lv.label(self.tile1)
-        self.tile1_label.set_text("Hello Students")
+        self.tile1_label.set_text("Hello Elsa")
         self.tile1_label.set_style_text_font(lv.font_montserrat_28, 0)
         self.tile1_label.center()
         self.apply_tile_colors(self.tile1, self.tile1_label, False)
